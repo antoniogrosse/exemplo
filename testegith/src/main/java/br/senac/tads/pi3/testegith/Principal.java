5139c2e64c43bd816da103870661a1703b762754
antonio.sgrosse <antonio.sgrosse@CAS511784W0710.senacsp.edu.br>
2017-02-21 21:27:20 -0300

    Mostrar somente se i for impar

--- testegith/src/main/java/br/senac/tads/pi3/testegith/Principal.java
+++ testegith/src/main/java/br/senac/tads/pi3/testegith/Principal.java
@@ -14,7 +14,11 @@ public class Principal {
     public static void main(String[] args) {
         
         for (int i = 0; i < 10; i++){
-          System.out.println("Hello Word " + i + "!!!");   
+            
+            if ( i% 2 == 1){
+                 System.out.println("Hello Word " + i + "!!!"); 
+            }
+            
         }
        
     }
