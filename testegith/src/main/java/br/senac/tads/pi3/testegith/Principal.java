a96917b131bcd5ac6d3e8ded112a687234a2bc02
antonio.sgrosse <antonio.sgrosse@CAS511784W0710.senacsp.edu.br>
2017-02-21 21:25:02 -0300

    Mostrar somente se i for par

--- testegith/src/main/java/br/senac/tads/pi3/testegith/Principal.java
+++ testegith/src/main/java/br/senac/tads/pi3/testegith/Principal.java
@@ -14,7 +14,10 @@ public class Principal {
     public static void main(String[] args) {
         
         for (int i = 0; i < 10; i++){
-          System.out.println("Hello Word " + i + "!!!");   
+          if (i % 2 == 0){
+            System.out.println("Hello Word " + i + "!!!");
+        }
+                  
         }
        
     }
